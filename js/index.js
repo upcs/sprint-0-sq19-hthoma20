479529031de2821e4fb1c8036b92f6096badba26
hthoma20 <thoma20@up.edu>
2019-01-30 00:38:44 -0800

    added package.json

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/*
+ * This document is copy-pasted from https://www.tutorialspoint.com/nodejs/nodejs_web_module.htm
+ */
+ 
+var http = require('http');
+
+// Options to be used by request 
+var options = {
+   host: 'localhost',
+   port: '8081',
+   path: '/index.html'  
+};
+
+// Callback function is used to deal with response
+var callback = function(response) {
+   // Continuously update stream with data
+   var body = '';
+   response.on('data', function(data) {
+      body += data;
+   });
+   
+   response.on('end', function() {
+      // Data received completely.
+      console.log(body);
+   });
+}
+// Make a request to the server
+var req = http.request(options, callback);
+req.end();
